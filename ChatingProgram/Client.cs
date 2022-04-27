dae6198bade2268e01cda6b41153c46aa07e9214
김주원 <juwon98999@naver.com>
2022-04-28 00:41:26 +0900

    초기 커밋.

---
+++ ChatingProgram/Client.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using System;
+
+
+public class Chat_Client
+{
+    public static void Main()
+    {
+        try
+        {
+            string server = "127.0.0.1";
+
+            Int32 port = 13000;
+            TcpClient client = new TcpClient(server, port);
+            Console.WriteLine("127.0.0.1:9000에 접속시도중...");
+            Console.WriteLine("'주'님께 연결되었습니다.");
+            String message = Console.ReadLine();
+
+
+            Byte[] data = System.Text.Encoding.Default.GetBytes(message);
+
+            NetworkStream stream = client.GetStream();
+
+            stream.Write(data, 0, data.Length);
+
+            Console.WriteLine($"[주] {message}");
+
+            data = new Byte[256];
+
+ 
+            String responseData = String.Empty;
+
+            Int32 bytes = stream.Read(data, 0, data.Length);
+            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+            Console.WriteLine($"Received: {responseData}");
+
+            // Close everything.
+            stream.Close();
+            client.Close();
+        }
+        catch (ArgumentNullException e)
+        {
+            Console.WriteLine("ArgumentNullException: {0}", e);
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("SocketException: {0}", e);
+        }
+
+        Console.WriteLine("\n Press Enter to continue...");
+        Console.Read();
+    }
+}
---
+++ ServerProgram/Server.cs
@@ -0,0 +1,69 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System;
+
+
+class Chat_Server
+{
+    public static void Main()
+    {
+        TcpListener server = null;
+        try
+        {
+
+            Int32 port = 13000;
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+            server = new TcpListener(localAddr, port);
+
+            Console.WriteLine("Waiting for a connection... ");
+
+            server.Start();
+            Console.WriteLine("'수' 님이 127.0.0.1에서 접속하셨습니다.");
+            String message = Console.ReadLine();
+            Console.WriteLine($"[수] {message}");
+
+
+            Byte[] bytes = new Byte[256];
+            String data = null;
+
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+
+                Console.ReadLine();
+
+                data = null;
+
+                NetworkStream stream = client.GetStream();
+
+                int i;
+
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine($"Received: {data}");
+
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                    stream.Write(msg, 0, msg.Length);
+                    Console.WriteLine($"Sent: {data}");
+                }
+
+                client.Close();
+            }
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("SocketException: {0}", e);
+        }
+        finally
+        {
+            server.Stop();
+        }
+
+        Console.WriteLine("\nHit enter to continue...");
+        Console.Read();
+    }
+}
